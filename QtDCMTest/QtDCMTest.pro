391eeb5d0eb59736ff890fd67befa875ebd2593c
realJeff <JeffMa2099@gmail.com>
2015-05-02 18:19:52 +1000

    A test program to test DCMTK in Qt

---
+++ QtDCMTest/QtDCMTest.pro
@@ -0,0 +1,54 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2015-05-01T21:20:08
+#
+#-------------------------------------------------
+
+QT       += core
+
+QT       -= gui
+
+TARGET = QtDCMTest
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+
+SOURCES += main.cpp
+
+
+INCLUDEPATH  += D:\programs\dcmtk\include
+INCLUDEPATH  += D:\programs\dcmtk\dcmdata\include
+INCLUDEPATH  += D:\programs\dcmtk\oflog\include
+INCLUDEPATH  += D:\programs\dcmtk\ofstd\include
+INCLUDEPATH  += D:\programs\dcmtk\dcmtk-supports\dcmtk-3.6.0-win32-i386-support_MD\libpng-1.4.3\include
+INCLUDEPATH  += D:\programs\dcmtk\dcmtk-supports\dcmtk-3.6.0-win32-i386-support_MD\libxml2-2.7.7\include
+INCLUDEPATH  += D:\programs\dcmtk\dcmtk-supports\dcmtk-3.6.0-win32-i386-support_MD\openssl-1.0.0c\include
+INCLUDEPATH  += D:\programs\dcmtk\dcmtk-supports\dcmtk-3.6.0-win32-i386-support_MD\tiff-3.9.4\include
+INCLUDEPATH  += D:\programs\dcmtk\dcmtk-supports\dcmtk-3.6.0-win32-i386-support_MD\zlib-1.2.5\include
+
+
+LIBS += D:\programs\dcmtk\Lib\Debug\AdvAPI32.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\oflog.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\ws2_32.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\netapi32.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\ofstd.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\dcmdata.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\dcmimgle.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\dcmimage.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\ijg8.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\ijg12.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\ijg16.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\dcmjpeg.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\dcmnet.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\dcmdsig.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\dcmsr.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\dcmtls.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\dcmpstat.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\dcmwlm.lib
+LIBS += D:\programs\dcmtk\Lib\Debug\dcmqrdb.lib
+
+
+
+
---
+++ QtDCMTest/main.cpp
@@ -0,0 +1,143 @@
+#include <QCoreApplication>
+#include "dcmtk/config/osconfig.h"
+#include "dcmtk/dcmdata/dctk.h"
+//#include <QTextStream>
+//#include <stdio.h>
+#include <QDebug>
+
+
+//overload operator<<, otherwise Qt would not recognize OFString.
+QDebug operator<<(QDebug dbg, const OFString &ofString)
+{
+    dbg.nospace() << ofString.c_str();
+    return dbg.space();
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    qDebug() << "************************Begin************************";
+
+//    QTextStream cin(stdin, QIODevice::ReadOnly);
+//    QTextStream cout(stdout, QIODevice::WriteOnly);
+//    QTextStream cerr(stderr, QIODevice::WriteOnly);
+
+
+    DcmFileFormat fileformat;
+    OFCondition status = fileformat.loadFile("test.dcm");
+    if (status.good())
+    {
+        OFString patientsName;
+        if (fileformat.getDataset()->findAndGetOFString(DCM_PatientName, patientsName).good())
+        {
+            qDebug() << "Patient's Name: " << patientsName;
+        }
+        else
+            qDebug() << "Error: cannot access Patient's Name!";
+
+//		OFString studyID;
+//		if (fileformat.getDataset()->findAndGetOFString(DCM_StudyID, studyID).good())
+//		{
+//			cout << "studyID: " << studyID << endl;
+//		}
+//		else
+//			cerr << "Error: cannot access studyID!" << endl;
+
+//		OFString seriesNumber;
+//		if (fileformat.getDataset()->findAndGetOFString(DCM_SeriesNumber, seriesNumber).good())
+//		{
+//			cout << "seriesNumber: " << seriesNumber << endl;
+//		}
+//		else
+//			cerr << "Error: cannot access seriesNumber!" << endl;
+
+//		OFString acquisitionNumber;
+//		if (fileformat.getDataset()->findAndGetOFString(DCM_AcquisitionNumber, acquisitionNumber).good())
+//		{
+//			cout << "acquisitionNumber: " << acquisitionNumber << endl;
+//		}
+//		else
+//			cerr << "Error: cannot access acquisitionNumber!" << endl;
+
+//		OFString instanceNumber;//Image No.
+//		if (fileformat.getDataset()->findAndGetOFString(DCM_InstanceNumber, instanceNumber).good())
+//		{
+//			cout << "instanceNumber: " << instanceNumber << endl;
+//		}
+//		else
+//			cerr << "Error: cannot access instanceNumber!" << endl;
+
+//		OFString windowCenter;
+//		if (fileformat.getDataset()->findAndGetOFString(DCM_WindowCenter, windowCenter).good())
+//		{
+//			cout << "WL: " << windowCenter << endl;
+//		}
+//		else
+//			cerr << "Error: cannot access windowCenter!" << endl;
+
+//		OFString windowWidth;
+//		if (fileformat.getDataset()->findAndGetOFString(DCM_WindowWidth, windowWidth).good())
+//		{
+//			cout << "WW: " << windowWidth << endl;
+//		}
+//		else
+//			cerr << "Error: cannot access windowWidth!" << endl;
+
+//		OFString rows;
+//		if (fileformat.getDataset()->findAndGetOFString(DCM_Rows, rows).good())
+//		{
+//			cout << "rows: " << rows << endl;
+//		}
+//		else
+//			cerr << "Error: cannot access rows!" << endl;
+
+//		OFString columns;
+//		if (fileformat.getDataset()->findAndGetOFString(DCM_Columns, columns).good())
+//		{
+//			cout << "columns: " << columns << endl;
+//		}
+//		else
+//			cerr << "Error: cannot access columns!" << endl;
+
+//		OFString numberOfFrames;
+//		if (fileformat.getDataset()->findAndGetOFString(DCM_NumberOfFrames, numberOfFrames).good())
+//		{
+//			cout << "numberOfFrames: " << numberOfFrames << endl;
+//		}
+//		else
+//			cerr << "Error: cannot access numberOfFrames!" << endl;
+
+//		OFString samplesPerPixel;
+//		if (fileformat.getDataset()->findAndGetOFString(DCM_SamplesPerPixel, windowWidth).good())
+//		{
+//			cout << "samplesPerPixel: " << samplesPerPixel << endl;
+//		}
+//		else
+//			cerr << "Error: cannot access samplesPerPixel!" << endl;
+
+//		OFString samplesPerPixelUsed;
+//		if (fileformat.getDataset()->findAndGetOFString(DCM_SamplesPerPixelUsed, windowWidth).good())
+//		{
+//			cout << "samplesPerPixelUsed: " << samplesPerPixelUsed << endl;
+//		}
+//		else
+//			cerr << "Error: cannot access samplesPerPixelUsed!" << endl;
+
+//		OFString photometricInterpretation;
+//		if (fileformat.getDataset()->findAndGetOFString(DCM_PhotometricInterpretation, windowWidth).good())
+//		{
+//			cout << "photometricInterpretation: " << photometricInterpretation << endl;
+//		}
+//		else
+//			cerr << "Error: cannot access photometricInterpretation!" << endl;
+    }
+    else
+        qDebug() << "Error: cannot read DICOM file (" << status.text() << ")";
+
+    qDebug() << "************************End************************";
+
+//    system("pause");
+
+    return a.exec();
+}
